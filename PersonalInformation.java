769abb20c5a22a406b3ed08359ae6a7879c4ff63
Nikita Baiborodov <157666959+Torinad2@users.noreply.github.com>
2025-05-04 16:56:47 -0400

    Add files via upload

---
+++ PersonalInformation.java
@@ -0,0 +1,30 @@
+/**************************************************************************************************
+ * Assignment: Personal Information                                                               *
+ *                                                                                                *
+ * Write a program that displays the following information, each on a separate line:              *
+ * • Your name                                                                                    *
+ * • Your address, with city, state, and ZIP                                                      *
+ * • Your telephone number                                                                        *
+ * • Your college major                                                                           *
+ * Although these items should be displayed on separate output lines, use only a single println   *
+ * statement in your program.                                                                     *
+ **************************************************************************************************/
+
+public class PersonalInformation {
+    public static void main(String[] args) {
+
+        /* ********************************************
+         * Display personal information using \n      *
+         **********************************************/
+        System.out.println("Nikita Baiborodov\n"
+                + "188 Richards Ave, Norwalk, CT 06854\n"
+                + "(203) 857-7000\n"
+                + "Computer Science: Software Development");
+
+        /* *********************************************
+         * Display developer credit                    *
+         ***********************************************/
+        System.out.println("\nDeveloped by: Nikita Baiborodov");
+    }
+}
+
